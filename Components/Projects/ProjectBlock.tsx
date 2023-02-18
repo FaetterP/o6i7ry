de8442203cbbcf846c868f0fd7911034dfb15698
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-02-18 22:10:01 +0300

    Добавлен блок для превью проекта

---
+++ Components/Projects/ProjectBlock.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import styles from "./ProjectBlock.module.scss";
+
+type PropsType = {
+  src: string;
+  title: string;
+  type: string;
+};
+
+export default function ProjectBlock(props: PropsType) {
+  return (
+    <>
+      <div className={styles.projectPreview}>
+        <div style={{display:"flex"}}>
+          <h2>{props.title}</h2> //{props.type}
+        </div>
+        <Image src={props.src} alt={props.title} width={180} height={180} />
+      </div>
+    </>
+  );
+}
---
+++ pages/projects.tsx
@@ -0,0 +1,11 @@
+import ProjectBlock from "Components/Projects/ProjectBlock";
+
+export default function Projects() {
+  return (
+    <>
+      <div>
+        <ProjectBlock title="title" type="type" src="/nuggetDiamond.png" />
+      </div>
+    </>
+  );
+}
